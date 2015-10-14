e86d63a92faf2e0a8eb2bb7a549b95c3f6a45848
Mark Gao <marc7115@gmail.com>
2015-10-14 15:13:57 +0800

    AggVal.h file

---
+++ scripts/AggVal.h
@@ -0,0 +1,20 @@
+// aggregate values
+struct AggVal {
+
+	int key = 0;
+	long sum = 0;
+	long count = 0;
+	double mean = 0;
+
+	double median = 0;
+
+	int lowest = 9999;
+	int highest = 0;
+
+	double q1 = 0;
+	double q3 = 0;
+
+	vector<long> qualVals;
+	AggVal() :qualVals(50,0){}
+
+} ;

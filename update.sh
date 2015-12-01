4370961cb450c83ef60e1cafb5393fdc98f1fda4
anarkia7115 <marc7115@gmail.com>
2015-12-01 15:46:33 +0800

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
-aws s3 cp ./bin/qc s3://gcbibucket/copy/qc
-ssh caws aws s3 cp s3://gcbibucket/copy/qc /gcbi/product/sequencing-analyze/QualityCheck/bin/
-
 scp ./bin/qc caws:/gcbi/product/sequencing-analyze/QualityCheck/bin/
 ssh caws chmod u+x /gcbi/product/sequencing-analyze/QualityCheck/bin/*
 

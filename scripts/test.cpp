04adabfb9612a07fb5e5e80374f7c4b0642731c7
shawn <shawn24601@gmail.com>
2016-08-11 20:23:35 +0800

    change test case

--- scripts/test.cpp
+++ scripts/test.cpp
@@ -2,16 +2,64 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <utility>
+#include <map>
 #include "./BedFile.h"
+#include "./GuessEncoding.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
 
-    string bedFilePath = "../data/S03723314_Covered_hg38.bed";
-    BedFile bf(bedFilePath);
+    string base33Fq = "../data/fq/base33.fq";
+    string base64Fq = "../data/fq/base64.fq";
 
-    long gnomeBaseTotal = bf.sumRegionDiff();
-    cout << gnomeBaseTotal << endl;
+    string line;
+
+
+    // line Pos from 0 to 3
+    int linePos = 0;
+
+    ifstream fqStream(base64Fq);
+
+    GuessEncoding ge;
+
+    string encodingKey;
+
+    // check file open
+    if (fqStream.is_open()) {
+
+        // read quality line parse to GuessEncoding
+        while (getline (fqStream, line)) {
+
+            // only read quality line
+            if (linePos != 3) {
+                linePos++;
+                continue;
+            }
+            else {
+                linePos = 0;
+            }
+
+            // parse to ge
+            if (ge.findEncoding(line)) {
+                encodingKey = ge.getHitKey();
+                break;
+            }
+        }
+    }
+    else {
+        // file open fail
+        cout << "Unable to open file" << endl;
+    }
+
+    if (encodingKey.length() == 0) {
+        cerr << "encoding not found!"<< endl;
+        exit(1);
+    }
+    else {
+        cout << "encoding: " << encodingKey << endl;
+    }
+
     return 0;
 }
